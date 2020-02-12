3650b27c5b670880bfe6b01feefedf93c37bf48a
Elaine <ezsunshine@outlook.com>
2020-02-11 23:57:07 -0500

    First commit

---
+++ loggingLetters.js
@@ -0,0 +1,27 @@
+var sentence = "I love learning software development";
+
+for (var i=0; i < sentence.length; i++) {
+      if(sentence[i] !== " "){
+        console.log(sentence[i]);
+      }
+}
+
+
+var n = 25;
+var result = 0;
+
+if (n%2===0) {
+  result = result +10;
+} else {
+  result = result - n;
+}
+
+console.log(result);
+
+
+
+var n = 25;
+
+for (var sum = 0; sum  < n; sum++) {
+  console.log(sum)
+}
---
+++ luckySevens/luckySevens.js
@@ -0,0 +1,41 @@
+function luckySevens() {
+    var stBet = document.getElementById("startingBet").value;
+    var money = stBet;
+    var rolls = 0;
+    var diceSum;
+    var highestWinning = 0;
+    var rollsAtMax = 0;
+
+    if (money <= 0) {
+        alert("Starting bet must be greater than 0.");
+    }
+    else {
+      while (money > 0) {
+        rolls++;
+        dice1 = Math.floor(Math.random()*6)+1;
+        dice2 = Math.floor(Math.random()*6)+1;
+        diceSum = dice1 + dice2;
+
+          if(diceSum != 7){
+          money = money -1;
+          }
+          else {
+            money = money + 4;
+            if (money > highestWinning) {
+            highestWinning = money;
+            rollsAtMax = rolls;
+            }
+          }
+      }
+    }
+
+   document.getElementById("results").style.display = "block";
+   document.getElementById("playButton").innerText = "Play Again";
+   document.getElementById("startingBet").innerText = stBet;
+   document.getElementById("totalRolls").innerText = rolls;
+   document.getElementById("highestAmountWon").innerText = highestWinning;
+   document.getElementById("rollCountAtHighestAmountWon").innerText = rollsAtMax;
+   // We are returning false so that the form doesn't submit
+   // and so that we can see the results
+   return false;
+}
